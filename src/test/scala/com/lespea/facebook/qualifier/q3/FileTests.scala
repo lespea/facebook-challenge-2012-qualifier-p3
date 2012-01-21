5d295142ddcd2b650334cb1967b0a4cd91d4ac2f
Adam Lesperance <lespea@gmail.com>
2012-01-21 16:08:57 -0600

    Make sure the file parser / tester works

---
+++ src/test/scala/com/lespea/facebook/qualifier/q3/FileTests.scala
@@ -0,0 +1,34 @@
+package com.lespea.facebook.qualifier.q3
+
+import org.scalatest.WordSpec
+import org.scalatest.matchers.MustMatchers
+
+final class FileTests extends WordSpec with MustMatchers {
+  val answerMap = List(
+    "WELCOME TO FACEBOOK HACKERCUP" -> 1,
+    "CUP WITH LABEL HACKERCUP BELONGS TO HACKER" -> 2,
+    "QUICK CUTE BROWN FOX JUMPS OVER THE LAZY DOG" -> 1,
+    "MOVE FAST BE BOLD" -> 0,
+    "HACK THE HACKERCUP" -> 1)
+
+  val wanted = answerMap.zipWithIndex.map {
+    case (e, i) ⇒
+      (SolvedProblem(i, e._1, e._2), "Case #" + i + ": " + e._2)
+  }
+
+  val sampleFile = this.getClass.getClassLoader.getResource("givenProblems.txt")
+
+  "A problem file" should {
+    "correctly parse" when {
+      "given a file" in {
+        val answers = Solver.solveProblem(sampleFile.toURI)
+
+        answers zip wanted foreach {
+          case (a, w) ⇒
+            a must equal(w._1)
+            a.outStr must equal(w._2)
+        }
+      }
+    }
+  }
+}
