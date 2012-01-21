8d99149271669010a2bed35922a0a60ad9c7b563
Adam Lesperance <lespea@gmail.com>
2012-01-21 15:43:59 -0600

    Add a method to print the problem as required

--- src/main/scala/com/lespea/facebook/qualifier/q3/Problem.scala
+++ src/main/scala/com/lespea/facebook/qualifier/q3/Problem.scala
@@ -4,6 +4,7 @@ final case class Problem(num: Int, problem: String)
 
 final case class SolvedProblem(num: Int, problem: String, answer: Int) {
   def this(p: Problem, a: Int) = this(p.num, p.problem, a)
+  def outStr = "Test #%d: %d".format(num, answer)
 }
 final object SolvedProblem {
   def apply(p: Problem, a: Int) = new SolvedProblem(p, a)
